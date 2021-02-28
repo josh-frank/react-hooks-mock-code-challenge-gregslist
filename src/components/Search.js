106b3620915d1627e1f50f8a70b98dac93153cc8
Josh Frank <eurosewusa@gmail.com>
2021-02-27 21:37:20 -0500

    commit tree 44e6cea209a736f81e6c407f5e41ad020d6a4aa4

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-function Search() {
+function Search( { searchQuery, setSearchQuery } ) {
+
   function handleSubmit(e) {
     e.preventDefault();
     console.log("submitted");
@@ -12,12 +13,13 @@ function Search() {
         type="text"
         id="search"
         placeholder="search free stuff"
-        value={""}
-        onChange={(e) => console.log(e.target.value)}
+        value={ searchQuery }
+        onChange={ change => setSearchQuery( change.target.value ) }
       />
       <button type="submit">🔍</button>
     </form>
   );
+
 }
 
 export default Search;
